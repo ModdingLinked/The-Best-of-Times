b3660657078429793c48047cda2e91686568f09d
WallSoGB <11296448+WallSoGB@users.noreply.github.com>
2023-04-07 18:15:09 +0200

    Mobile button gets colored too

--- javascript.js
+++ javascript.js
@@ -1,41 +1,4 @@
 window.addEventListener('resize', sizeChanged);
-window.addEventListener('load', setBackgroundByTime);
-
-function setBackgroundByTime() {
-    const date = new Date();
-    const hour = date.getHours();
-    const headerTitle = document.getElementById('headerTitle');
-    var updateColor = 0;
-
-    // set the background image based on the current time
-    if (hour >= 0 && hour < 7) {
-        document.body.style.backgroundImage = "url('./img/background/img0.webp')";
-    } else if (hour >= 7 && hour < 9) {
-        document.body.style.backgroundImage = "url('./img/background/img7.webp')";
-        updateColor = 1;
-    } else if (hour >= 9 && hour < 12) {
-        document.body.style.backgroundImage = "url('./img/background/img9.webp')";
-        updateColor = 1;
-    } else if (hour >= 12 && hour < 17) {
-        document.body.style.backgroundImage = "url('./img/background/img12.webp')";
-        updateColor = 1;
-    } else if (hour >= 17 && hour < 20) {
-        document.body.style.backgroundImage = "url('./img/background/img17.webp')";
-        updateColor = 1;
-    } else if (hour >= 20 && hour < 23) {
-        document.body.style.backgroundImage = "url('./img/background/img20.webp')";
-    } else {
-        document.body.style.backgroundImage = "url('./img/background/img23.webp')";
-    }
-    const headerLinks = document.getElementById('headerLinks');
-
-    if (updateColor) {
-        headerTitle.style.color = "rgba(0, 0, 0, 0.75)";
-        Array.from(headerLinks.children).forEach(child => {
-            child.style.color = "rgba(0, 0, 0, 0.75)";
-        });
-    }
-}
 
 document.addEventListener('keyup', (e) => {
     if (e.code === "ArrowRight") {
