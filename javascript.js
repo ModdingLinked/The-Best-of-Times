639e95db728d492aaa5e880adc861ed4e6bc4796
WallSoGB <11296448+WallSoGB@users.noreply.github.com>
2024-07-15 22:51:44 +0200

    PWA test
    bored

--- javascript.js
+++ javascript.js
@@ -267,3 +267,26 @@ document.addEventListener('click', function (event) {
         }
     }
 });
+
+const registerServiceWorker = async () => {
+    if ("serviceWorker" in navigator) {
+        try {
+            const registration = await navigator.serviceWorker.register("./service-worker.js", {
+                scope: "/",
+            });
+            if (registration.installing) {
+                console.log("Service worker installing");
+            } else if (registration.waiting) {
+                console.log("Service worker installed");
+            } else if (registration.active) {
+                console.log("Service worker active");
+            }
+        } catch (error) {
+            console.error(`Registration failed with ${error}`);
+        }
+    }
+};
+
+// …
+
+registerServiceWorker();
